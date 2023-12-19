e8ff1438c0872a5efb34c06bf3cce4dc5a7fa8cb
Spenser Bauman <sabauma@fastmail.com>
2023-12-19 07:46:45 -0500

    Add xmonad configuration to home-manager

---
+++ xmonad/lib/PerWorkspaceDirs.hs
@@ -0,0 +1,33 @@
+{-# OPTIONS_GHC -O2 -Wall               #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-------------------------------------------------------------------------------
+-- | Allows per workspace working directories
+-- ----------------------------------------------------------------------------
+
+module PerWorkspaceDirs
+  ( currentWorkspace
+  , changeDir
+  , getDir
+  ) where
+
+import           Data.Map                    as M
+import           Data.Maybe                  (fromMaybe)
+import           XMonad.Core
+import           XMonad.StackSet             (currentTag)
+import qualified XMonad.Util.ExtensibleState as XS
+
+newtype WorkingDirs = WD { unWD :: M.Map WorkspaceId String }
+  deriving (Read, Show, Typeable)
+
+instance ExtensionClass WorkingDirs where
+  initialValue  = WD M.empty
+  extensionType = PersistentExtension
+
+changeDir :: String -> WorkspaceId -> X ()
+changeDir dir ws = XS.modify (WD . M.insert ws dir . unWD)
+
+getDir :: WorkspaceId -> X String
+getDir ws = fromMaybe "~/" . M.lookup ws . unWD <$> XS.get
+
+currentWorkspace :: X WorkspaceId
+currentWorkspace = withWindowSet (return . currentTag)
