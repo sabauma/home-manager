c20908c3e045c98b928f68a551e09de8093dd977
Spenser Bauman <sbauman@mathworks.com>
2024-01-12 17:24:08 -0500

    Reinstate a bordered layout

--- nvim/config.lua
+++ nvim/config.lua
@@ -126,22 +126,15 @@ local lspkind = require('lspkind')
 cmp.setup {
   snippet = { },
 
-  -- window = {
-  --   completion = cmp.config.window.bordered({
-  --     col_offset = -3,
-  --     side_padding = 0,
-  --   }),
-
-  --   documentation = cmp.config.window.bordered({
-  --     col_offset = -3,
-  --     side_padding = 0,
-  --   }),
-  -- },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
 
   formatting = {
     fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
-      local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 65 })(entry, vim_item)
+      local kind = lspkind.cmp_format({ mode = "symbol_text", maxwidth = 65 })(entry, vim_item)
       local strings = vim.split(kind.kind, "%s", { trimempty = true })
       kind.kind = " " .. (strings[1] or "") .. " "
       kind.menu = "    (" .. (strings[2] or "") .. ")"
