ee17baed503ceb90cdad2a6ab29a386b74c10e5c
Spenser Bauman <sabauma@fastmail>
2024-01-17 22:07:32 -0500

    Add a patched version of obsidian

--- home.nix
+++ home.nix
@@ -1,5 +1,17 @@
 { config, pkgs, lib, ... }:
 
+let
+  obsidian-patched = lib.throwIf (lib.versionOlder "1.4.16" pkgs.obsidian.version) "Obsidian no longer requires EOL Electron" (
+    pkgs.obsidian.override {
+      electron = pkgs.electron_25.overrideAttrs (_: {
+        preFixup = "patchelf --add-needed ${pkgs.libglvnd}/lib/libEGL.so.1 $out/bin/electron"; # NixOS/nixpkgs#272912
+        meta.knownVulnerabilities = [ ]; # NixOS/nixpkgs#273611
+      });
+    }
+  );
+
+in
+
 {
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
