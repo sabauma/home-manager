487fdafe1cf27a5c06ea659cac1abcf0caea1a31
Spenser Bauman <sabauma@fastmail>
2024-04-11 18:19:48 -0400

    Flake update

--- flake.nix
+++ flake.nix
@@ -10,23 +10,21 @@
     };
 
     flake-utils.url = "github:numtide/flake-utils";
-
-    neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
-
     mlir-nix.url = "github:sabauma/mlir.nix";
+    neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
   };
 
   outputs = { self, nixpkgs, home-manager, flake-utils, neovim-nightly, mlir-nix, ... }@inputs:
-    {
-      homeConfigurations.spenser = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [ ./home.nix ];
+  {
+    homeConfigurations.spenser = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      modules = [ ./home.nix ];
 
-        extraSpecialArgs = {
-          inherit neovim-nightly;
-          inherit mlir-nix;
-        };
+      extraSpecialArgs = {
+        inherit neovim-nightly;
+        inherit mlir-nix;
       };
     };
+  };
 }
 
