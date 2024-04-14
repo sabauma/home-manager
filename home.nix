c8aa6fc0defc537cb15f335c4c83261157cf60b2
Spenser Bauman <sabauma@fastmail>
2024-04-14 15:01:56 -0400

    Factor out starship into its own module

--- home.nix
+++ home.nix
@@ -274,53 +274,6 @@
       };
     };
 
-    starship = {
-      enable = true;
-      settings = {
-        # Don't print a new line at the start of the prompt
-        add_newline = false;
-
-        # Wait 10 milliseconds for starship to check files under the current directory.
-        scan_timeout = 10;
-        command_timeout = 500;
-
-        username = {
-          style_user = "blue";
-          style_root = "bold black";
-          format = "[$user]($style)";
-          disabled = false;
-          show_always = true;
-        };
-
-        time = {
-          disabled = false;
-          style = "cyan";
-          format = "[\\[ $time \\]]($style) ";
-          time_format = "%T";
-          utc_time_offset = "local";
-        };
-
-        hostname = {
-          ssh_only = false;
-          format =  "[@](dimmed red)[$hostname](bold green) ";
-          trim_at = ".companyname.com";
-          disabled = false;
-        };
-
-        directory = {
-          truncation_length = 8;
-        };
-
-        # Disable annoying features
-        cmd_duration = { disabled = true; };
-        python = { disabled = true; };
-        julia = { disabled = true; };
-        conda = { disabled = true; };
-        vlang = { disabled = true; };
-        nodejs = { disabled = true; };
-      };
-    };
-
     fish = {
       enable = true;
       plugins = [
@@ -377,7 +330,6 @@
               split-window -l 10 "urlview /tmp/tmux-buffer"
       '';
     };
-
 
     xmobar = {
       enable = true;
---
+++ starship.nix
@@ -0,0 +1,50 @@
+{ pkgs, ... }:
+
+{
+  programs.starship = {
+    enable = true;
+    settings = {
+      # Don't print a new line at the start of the prompt
+      add_newline = false;
+
+      # Wait 10 milliseconds for starship to check files under the current directory.
+      scan_timeout = 10;
+      command_timeout = 500;
+
+      username = {
+        style_user = "blue";
+        style_root = "bold black";
+        format = "[$user]($style)";
+        disabled = false;
+        show_always = true;
+      };
+
+      time = {
+        disabled = false;
+        style = "cyan";
+        format = "[\\[ $time \\]]($style) ";
+        time_format = "%T";
+        utc_time_offset = "local";
+      };
+
+      hostname = {
+        ssh_only = false;
+        format =  "[@](dimmed red)[$hostname](bold green) ";
+        trim_at = ".companyname.com";
+        disabled = false;
+      };
+
+      directory = {
+        truncation_length = 8;
+      };
+
+      # Disable annoying features
+      cmd_duration = { disabled = true; };
+      python = { disabled = true; };
+      julia = { disabled = true; };
+      conda = { disabled = true; };
+      vlang = { disabled = true; };
+      nodejs = { disabled = true; };
+    };
+  };
+}
