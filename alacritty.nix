556faae04b2d4886a6f45abafd880a4d384716fa
Spenser Bauman <sabauma@fastmail>
2024-04-15 22:28:17 -0400

    Factor out alacritty

---
+++ alacritty.nix
@@ -0,0 +1,59 @@
+{ pkgs, ... }:
+{
+  programs.alacritty = {
+    enable = true;
+    package = pkgs.alacritty;
+    settings = {
+      font = {
+        size = 10;
+        normal = {
+          family = "Development";
+          style = "Regular";
+        };
+        bold = {
+          family = "Development";
+          style = "Bold";
+        };
+        italic = {
+          family = "Development";
+          style = "Italic";
+        };
+        bold_italic = {
+          family = "Development";
+          style = "Bold Italic";
+        };
+      };
+
+      colors.draw_bold_text_with_bright_colors = false;
+
+      colors.primary = {
+        background = "0x1d2021";
+        foreground = "0xebdbb2";
+      };
+
+      colors.normal = {
+        black   = "0x282828";
+        red     = "0xcc241d";
+        green   = "0x98971a";
+        yellow  = "0xd79921";
+        blue    = "0x458588";
+        magenta = "0xb16286";
+        cyan    = "0x689d6a";
+        white   = "0xa89984";
+      };
+
+      colors.bright = {
+        black   = "0x928374";
+        red     = "0xfb4934";
+        green   = "0xb8bb26";
+        yellow  = "0xfabd2f";
+        blue    = "0x83a598";
+        magenta = "0xd3869b";
+        cyan    = "0x8ec07c";
+        white   = "0xebdbb2";
+      };
+
+      shell = "${pkgs.fish}/bin/fish";
+    };
+  };
+}
--- home.nix
+++ home.nix
@@ -81,7 +81,6 @@
     htop
     hyperfine
     mosh
-    neovim-nightly
     newsboat
     openconnect
     pyright
@@ -101,7 +100,6 @@
     fish
 
     # Graphical programs
-    alacritty
     calibre
     chromium
     discord
@@ -171,63 +169,6 @@
   };
 
   programs = {
-    alacritty = {
-      enable = true;
-      package = pkgs.alacritty;
-      settings = {
-        font = {
-          size = 8;
-          normal = {
-            family = "Development";
-            style = "Regular";
-          };
-          bold = {
-            family = "Development";
-            style = "Bold";
-          };
-          italic = {
-            family = "Development";
-            style = "Italic";
-          };
-          bold_italic = {
-            family = "Development";
-            style = "Bold Italic";
-          };
-        };
-
-        colors.draw_bold_text_with_bright_colors = false;
-
-        colors.primary = {
-          background = "0x1d2021";
-          foreground = "0xebdbb2";
-        };
-
-        colors.normal = {
-          black   = "0x282828";
-          red     = "0xcc241d";
-          green   = "0x98971a";
-          yellow  = "0xd79921";
-          blue    = "0x458588";
-          magenta = "0xb16286";
-          cyan    = "0x689d6a";
-          white   = "0xa89984";
-        };
-
-        colors.bright = {
-          black   = "0x928374";
-          red     = "0xfb4934";
-          green   = "0xb8bb26";
-          yellow  = "0xfabd2f";
-          blue    = "0x83a598";
-          magenta = "0xd3869b";
-          cyan    = "0x8ec07c";
-          white   = "0xebdbb2";
-        };
-
-        shell = "${pkgs.fish}/bin/fish";
-      };
-    };
-
     atuin = {
       enable = true;
     };
