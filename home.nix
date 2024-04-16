43fc9c54c0f56adfe5c772a36f6f5cae6429e375
Spenser Bauman <sabauma@fastmail>
2024-04-15 22:28:17 -0400

    Factor out alacritty

--- home.nix
+++ home.nix
@@ -10,6 +10,7 @@ in
   ];
 
   imports = [
+    ./alacritty.nix
     ./neovim.nix
     ./starship.nix
   ];
@@ -98,7 +99,6 @@ in
     htop
     hyperfine
     mosh
-    neovim-nightly
     newsboat
     ninja
     openconnect
@@ -122,7 +122,6 @@ in
     fish
 
     # Graphical programs
-    alacritty
     calibre
     chromium
     discord
@@ -192,63 +191,6 @@ in
   };
 
   programs = {
-    alacritty = {
-      enable = true;
-      package = pkgs.alacritty;
-      settings = {
-        font = {
-          size = 10;
-          normal = {
-            family = "Development";
-            style = "Regular";
-          };
-          bold = {
-            family = "Development";
-            style = "Bold";
-          };
-          italic = {
-            family = "Development";
-            style = "Italic";
-          };
-          bold_italic = {
-            family = "Development";
-            style = "Bold Italic";
-          };
-        };
-
-        colors.draw_bold_text_with_bright_colors = false;
-
-        colors.primary = {
-          background = "0x1d2021";
-          foreground = "0xebdbb2";
-        };
-
-        colors.normal = {
-          black   = "0x282828";
-          red     = "0xcc241d";
-          green   = "0x98971a";
-          yellow  = "0xd79921";
-          blue    = "0x458588";
-          magenta = "0xb16286";
-          cyan    = "0x689d6a";
-          white   = "0xa89984";
-        };
-
-        colors.bright = {
-          black   = "0x928374";
-          red     = "0xfb4934";
-          green   = "0xb8bb26";
-          yellow  = "0xfabd2f";
-          blue    = "0x83a598";
-          magenta = "0xd3869b";
-          cyan    = "0x8ec07c";
-          white   = "0xebdbb2";
-        };
-
-        shell = "${pkgs.fish}/bin/fish";
-      };
-    };
-
     atuin = {
       enable = true;
     };
