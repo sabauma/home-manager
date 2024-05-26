140e10f918018ec9b2b5a9fdf23338e7807d8daa
Spenser Bauman <sabauma@fastmail>
2024-05-26 08:13:27 -0400

    Add zellij to the config

--- home.nix
+++ home.nix
@@ -308,6 +308,28 @@ in
       enable = true;
       extraConfig = builtins.readFile ./xmobarrc;
     };
+
+    zellij = {
+      enable = true;
+      settings = {
+        default_shell = "${pkgs.fish}/bin/fish";
+        themes = {
+          gruvbox-dark = {
+            fg = "#D5C4A1";
+            bg = "#282828";
+            black = "#3C3836";
+            red = "#CC241D";
+            green = "#98971A";
+            yellow = "#D79921";
+            blue = "#3C8588";
+            magenta = "#B16286";
+            cyan = "#689D6A";
+            white = "#FBF1C7";
+            orange = "#D65D0E";
+          };
+        };
+      };
+    };
   };
 
   services.picom = {
