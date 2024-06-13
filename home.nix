8204ff8feb6463eb8c5fef97a1ce79ec7711f75e
Spenser Bauman <sabauma@fastmail>
2024-06-13 07:52:10 -0400

    Factor out tmux config

--- home.nix
+++ home.nix
@@ -14,6 +14,7 @@ in
     ./alacritty.nix
     ./neovim.nix
     ./starship.nix
+    ./tmux.nix
     ./xmonad/xmonad.nix
   ];
 
@@ -261,47 +262,6 @@ in
         sorting-method  = "fzf";
         terminal = "alacritty";
       };
-    };
-
-    tmux = {
-      enable = true;
-
-      aggressiveResize = true;
-      customPaneNavigationAndResize = true;
-      baseIndex = 1;
-      escapeTime = 0;
-      historyLimit = 50000;
-      keyMode = "vi";
-      mouse = true;
-      terminal = "xterm-256color";
-      shell = "${pkgs.fish}/bin/fish";
-
-      plugins = with pkgs.tmuxPlugins; [
-        gruvbox
-        prefix-highlight
-        tmux-fzf
-      ];
-
-      extraConfig = ''
-      bind '"' split-window -c "#{pane_current_path}"
-      bind % split-window -h -c "#{pane_current_path}"
-
-      # some nice pane navigation settings
-      bind-key h select-pane -L
-      bind-key j select-pane -D
-      bind-key k select-pane -U
-      bind-key l select-pane -R
-
-      bind-key -n C-h select-pane -L
-      bind-key -n C-j select-pane -D
-      bind-key -n C-k select-pane -U
-      bind-key -n C-l select-pane -R
-
-      # present a menu of URLs to open from the visible pane. sweet.
-      bind-key u capture-pane \;\
-          save-buffer /tmp/tmux-buffer \;\
-              split-window -l 10 "urlview /tmp/tmux-buffer"
-      '';
     };
 
     xmobar = {
