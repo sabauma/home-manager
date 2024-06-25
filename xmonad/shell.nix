88faf78f8e1ade9542e3693bca6e4480f7fe038d
Spenser Bauman <sabauma@fastmail>
2024-06-25 07:47:48 -0400

    Add a shell environment for developing xmonad configuration

---
+++ xmonad/shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  haskellEnv = pkgs.haskellPackages.ghcWithPackages (ps: with ps; [
+    hashable
+
+    text
+    text-icu
+
+    xmonad
+    xmonad-contrib
+    xmonad-extras
+    vector
+  ]);
+in
+pkgs.mkShell {
+  packages = [
+    haskellEnv
+  ];
+}
