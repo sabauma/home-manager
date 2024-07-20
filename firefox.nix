bf8cbce33a504399dfe6a9b2eac81c02521a4da4
Spenser Bauman <sabauma@fastmail>
2024-07-19 21:10:10 -0400

    Firefox privacy configuration

--- firefox.nix
+++ firefox.nix
@@ -7,7 +7,41 @@
     policies = {
       # Disable private attribution, Mozilla's "privacy preserving"
       # ad/tracking APIs.
-      Preferences."dom.private-attribution.submission.enabled" = false;
+      Preferences = {
+        "dom.private-attribution.submission.enabled" = false;
+        "privacy.annotate_channels.strict_list.enabled" = true;
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.fingerprintingProtection" = true;
+        "privacy.globalprivacycontrol.enabled" = true;
+        "privacy.globalprivacycontrol.was-ever-enabled" = true;
+        "privacy.partition.network_state.ocsp_cache" = true;
+        "privacy.popups.showBrowserMessage" = false;
+        "privacy.query_stripping.enabled" = true;
+        "privacy.query_stripping.enabled.pbmode" = true;
+        "privacy.resistFingerprinting" = true;
+        "privacy.trackingprotection.emailtracking.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+        "privacy.userContext.enabled" = true;
+
+        "toolkit.coverage.endpoint.base" = "";
+        "toolkit.coverage.opt-out" = true;
+        "toolkit.telemetry.archive.enabled" = false;
+        "toolkit.telemetry.bhrPing.enabled" = false;
+        "toolkit.telemetry.coverage.opt-out" = true;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "toolkit.telemetry.newProfilePing.enabled" = false;
+        "toolkit.telemetry.pioneer-new-studies-available" = false;
+        "toolkit.telemetry.reportingpolicy.firstRun" = false;
+        "toolkit.telemetry.server" = "data:";
+        "toolkit.telemetry.shutdownPingSender.enabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
+        "trailhead.firstrun.didSeeAboutWelcome" = true;
+      };
+
+      PopupBlocking.Default = true;
 
       DisableFirefoxStudies = true;
       DisablePocket = true;
