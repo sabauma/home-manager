d8668c86e60296d03de22540375840c9dd6f3f49
Spenser Bauman <sabauma@fastmail>
2024-08-22 11:29:44 -0400

    Add setup script

---
+++ misc/setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This script sets up a machine for using my personal home-manager configuration
+
+# Install nix
+sh <(curl -L https://nixos.org/nix/install) --daemon
+
+# Install home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
+
+# Check out my home-manager installation
+rm -rf ~/.config/home-manager
+git clone https://github.com/sabauma/home-manager.git ~/.config/home-manager
+
+# Build home-manager config
+pushd ~/.config/home-manager
+git checkout modular-laptop
+home-manager --extra-experimental-features nix-command --extra-experimental-features flakes switch --flake ".#spenser"
+popd
