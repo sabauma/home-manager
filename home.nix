ddb857c95561836c90a0d3a3db8a8b85956caaee
Spenser Bauman <sabauma@fastmail.com>
2024-08-23 13:01:44 +0000

    Make fish use vim keybindings by default

--- home.nix
+++ home.nix
@@ -281,7 +281,11 @@ in
       ];
 
       functions = {
+        fish_key_bindings = "fish_vi_key_bindings";
+
         fish_user_key_bindings = ''
+          fish_vi_key_bindings
+
           for mode in insert default visual
               bind -M $mode \cf forward-char
           end
