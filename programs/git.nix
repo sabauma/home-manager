71659a29dd8cf0c3aa9b52de8455647b3b8cf060
Spenser Bauman <sabauma@fastmail>
2024-11-09 10:00:57 -0500

    More git configuration

--- programs/git.nix
+++ programs/git.nix
@@ -6,7 +6,38 @@
     userName = "Spenser Bauman";
 
     # Enable difftastic integration
-    difftastic.enable = true;
-    difftastic.background = "dark";
+    difftastic = {
+      enable = true;
+      background = "dark";
+    };
+
+    extraConfig = {
+      # Sort branches by commit date rather than alphabetically
+      branch.sort = "-committerdate";
+
+      # Better diff algorithm
+      diff.algorithm = "histogram";
+
+      # Default to 'main' over 'master'
+      init.defaultBranch = "main";
+
+      # Better conflict format
+      merge.conflictstyle = "zdiff3";
+
+      # Don't make me set up the remote
+      push.autoSetupRemote = true;
+
+      # Don't make me stash before a rebase
+      rebase.autostash = true;
+
+      # Automatically delete remote tracking branches that have been deleted
+      fetch.prune = true;
+      fetch.prunetags = true;
+
+      # Each checking of repo state
+      fetch.fsckobjects = true;
+      receive.fsckObjects = true;
+      transfer.fsckobjects = true;
+    };
   };
 }
