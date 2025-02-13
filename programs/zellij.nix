35db45b5e2c262f421019a613f78db8d4d2b96dd
Spenser Bauman <sabauma@fastmail>
2025-02-13 08:49:14 -0500

    Disable fish integration of zellij

--- programs/zellij.nix
+++ programs/zellij.nix
@@ -2,6 +2,8 @@
 {
   programs.zellij = {
     enable = true;
+    enableBashIntegration = false;
+    enableFishIntegration = false;
     settings = {
       default_layout = "compact";
       default_shell = "${pkgs.fish}/bin/fish";
