c3638984beef24fb0ac3f0e94239f98c8c31998e
Spenser Bauman <spenser@modular.com>
2025-05-05 09:03:24 -0400

    Add shell.nix for use with direnv

---
+++ misc/shell.nix
@@ -0,0 +1,41 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  # Function to create script
+  mkScript = pkgs.writeShellScriptBin;
+
+  # Needed to create shell scripts which reference the current directory.
+  # We don't want to pull all the related shell scripts into the nix store,
+  # particularly bazelw, which inspecs the directory the script is in to figure
+  # out where to store things.
+  baseDir = toString ./.;
+
+  # The root bazel commands that we will execute.
+  bazel = mkScript "bazel" ''${baseDir}/bazelw "$@"'';
+  bazelw = mkScript "bazelw" ''${baseDir}/bazelw "$@"'';
+
+  # The bazel command.
+  bazelCmd = ''${bazel}/bin/bazel'';
+
+  # Aliases we actually use. Direnv cannot export aliases, so we create a
+  # derivation for the relevant aliases, which will be added to $PATH.
+  # This is more robust than alises anyway.
+  scripts = [
+    bazel
+    bazelw
+
+    (mkScript "bb" ''${bazelCmd} build "$@"'')
+    (mkScript "bd" ''./utils/bazel-debug.sh "$@"'')
+    (mkScript "bqg" ''${bazelCmd} query "..." | grep "$@"'')
+    (mkScript "br" ''${bazelCmd} run "$@"'')
+    (mkScript "model" ''${bazelCmd} run -- //GenericML/tool/model:modeltool "$@"'')
+  ];
+in
+  pkgs.mkShell {
+    name = "Development shell";
+
+    nativeBuildInputs = [ ] ++ scripts;
+
+    shellHook = ''
+      source ./utils/start-modular.sh
+    '';
+  }
