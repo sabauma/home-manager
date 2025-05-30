d8e541779115756153fb54997abf0e56b0c7a2e4
Spenser Bauman <spenser@modular.com>
2025-05-30 08:55:40 -0400

    Update shell.nix for Modular development

--- misc/shell.nix
+++ misc/shell.nix
@@ -1,6 +1,4 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
+{ pkgs ? import <nixpkgs> {} }:
 let
   # Function to create script
   mkScript = pkgs.writeShellScriptBin;
@@ -15,7 +13,7 @@ let
   bazel = mkScript "bazel" ''${baseDir}/bazelw "$@"'';
   bazelw = mkScript "bazelw" ''${baseDir}/bazelw "$@"'';
 
-  # The bazel command.
+  # Execute the bazel command.
   bazelCmd = ''${bazel}/bin/bazel'';
 
   # Aliases we actually use. Direnv cannot export aliases, so we create a
@@ -29,15 +27,22 @@ let
     (mkScript "bd" ''./utils/bazel-debug.sh "$@"'')
     (mkScript "bqg" ''${bazelCmd} query "..." | grep "$@"'')
     (mkScript "br" ''${bazelCmd} run "$@"'')
+    (mkScript "bar" ''${bazelCmd} run "$@"'')
     (mkScript "model" ''${bazelCmd} run -- //GenericML/tool/model:modeltool "$@"'')
   ];
 in
-pkgs.mkShell {
-  name = "Development shell";
+  pkgs.mkShell {
+    name = "modular-dev";
 
-  nativeBuildInputs = [ ] ++ scripts;
+    nativeBuildInputs = [
+      pkgs.bazelisk
+      pkgs.bazel-watcher
+    ] ++ scripts;
 
-  shellHook = ''
-    source ./utils/start-modular.sh
-  '';
-}
+    shellHook = ''
+      # This will set up the relevant environment variables, but
+      # will not be able to export the aliases defined in `aliases.sh` through
+      # direnv.
+      source ./utils/start-modular.sh
+    '';
+  }
