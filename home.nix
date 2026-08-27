5c9f51add6c34be43212a05cf00691885f729936
Spenser Bauman <sabauma@fastmail>
2026-08-27 19:42:07 -0400

    Add opencode config with Modular provider

--- home.nix
+++ home.nix
@@ -228,6 +228,31 @@ in
       defaultOptions = [ ];
     };
 
+    opencode = {
+      enable = true;
+
+      settings = {
+        provider = {
+          modular = {
+            npm = "@ai-sdk/openai-compatible";
+            name = "Modular";
+            options = {
+              baseURL = "https://api.modular.com/v1";
+            };
+
+            models = {
+              "z-ai/glm-5.2" = {
+                name = "GLM 5.2";
+              };
+              "minimax/minimax-m3" = {
+                name = "Minimax m3";
+              };
+            };
+          };
+        };
+      };
+    };
+
     readline = {
       enable = true;
       extraConfig = ''
